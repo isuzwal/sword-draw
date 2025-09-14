3c0ee4acdf1f260932dcf2b3338286bc8c06149e
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-09-15 01:19:29 +0530

    have to workin  in the FE

--- apps/web-forntend/app/draw/index.ts
+++ apps/web-forntend/app/draw/index.ts
@@ -14,71 +14,76 @@ type Shape={
 }
 // take the argument 
 export  async function DrawInit(canvas:HTMLCanvasElement,roomId:string,socket:WebSocket){
-  const ctx=canvas.getContext("2d");
-  
-  // gobal array of the Shape with fetching all shape for db 
-  let exitingShape :Shape[]=await FetchingAllShape(roomId);
+  try{
 
-  if(!ctx){
-    return;
-  }
-  // sokcet connection
-   socket.onmessage=(event)=>{
-    const message=JSON.parse(event.data);
-    console.log("At thre message in scoket--->",message)
-    if(message==="chat"){
-      const parsedShape=JSON.parse(message.message)
-      exitingShape.push(parsedShape); 
-      // re-render the canvas after something add to state
-      clearCnavas(exitingShape ,canvas,ctx);
+    const ctx=canvas.getContext("2d");
+    
+    // gobal array of the Shape with fetching all shape for db 
+    let exitingShape :Shape[]=await FetchingAllShape(roomId);
+    
+    if(!ctx){
+      return;
     }
-   }
- clearCnavas(exitingShape ,canvas,ctx);
-  let clicked=false;
-  let startX=0;
-  let startY=0;
-  canvas.addEventListener("mousedown",(e)=>{
-    clicked=true;
-    startX=e.clientX;
-    startY=e.clientY;
-  })
-  // when mouse is up
-  canvas.addEventListener("mouseup",(e)=>{
-    // have to fix scroolball options during canvas hight change 
-    clicked=false;
-    // re-cal of the hight  when the hight change
-    const width=e.clientX-startX;
-    const height=e.clientY-startY; 
-    // push the shape of the shape after the  mouse-up 
-    const shape:Shape={
-       type:"rectangle",
+    // sokcet connection
+    socket.onmessage=(event)=>{
+      const message=JSON.parse(event.data);
+      console.log("At thre message in scoket--->",message)
+      if(message==="chat"){
+        const parsedShape=JSON.parse(message.message)
+        exitingShape.push(parsedShape); 
+        // re-render the canvas after something add to state
+        clearCnavas(exitingShape ,canvas,ctx);
+      }
+    }
+    clearCnavas(exitingShape ,canvas,ctx);
+    let clicked=false;
+    let startX=0;
+    let startY=0;
+    canvas.addEventListener("mousedown",(e)=>{
+      clicked=true;
+      startX=e.clientX;
+      startY=e.clientY;
+    })
+    // when mouse is up
+    canvas.addEventListener("mouseup",(e)=>{
+      // have to fix scroolball options during canvas hight change 
+      clicked=false;
+      // re-cal of the hight  when the hight change
+      const width=e.clientX-startX;
+      const height=e.clientY-startY; 
+      // push the shape of the shape after the  mouse-up 
+      const shape:Shape={
+        type:"rectangle",
         x:startX,
         y:startY,
         width,
         height,
-    }
-    // sending the socket backend 
-    exitingShape.push(shape)
-    socket.send(JSON.stringify({
-      type:"chat",
-       message:JSON.stringify({
-        shape
-       })
-    }))
-  })
-  canvas.addEventListener("mousemove",(e)=>{
- 
-    if(clicked){
+      }
+      // sending the socket backend 
+      exitingShape.push(shape)
+      socket.send(JSON.stringify({
+        type:"chat",
+        message:JSON.stringify({
+          shape
+        })
+      }))
+    })
+    canvas.addEventListener("mousemove",(e)=>{
+      
+      if(clicked){
         const width=e.clientX-startX;
         const height=e.clientY-startY;      
-         ctx.fillStyle="rgba(18,18,18,18)"; 
+        ctx.fillStyle="rgba(18,18,18,18)"; 
         // fill with new one
-         clearCnavas(exitingShape,canvas,ctx)
+        clearCnavas(exitingShape,canvas,ctx)
         ctx.strokeStyle="rgba(255,255,255,1)";
-           ctx.lineWidth = 1;
+        ctx.lineWidth = 1;
         ctx.strokeRect(startX,startY,width,height)
-    }
-  })
+      }
+    })
+  }catch(err){
+    console.log("Error At the Drawing part inde.ts--->",err)
+  }
 }
 
 function clearCnavas(exitingShape:Shape[], canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D){

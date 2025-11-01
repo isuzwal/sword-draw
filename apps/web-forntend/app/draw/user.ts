09a9c73ada289c817b6b4b9079e721de15322ee7
Ujjwal Gaihre <ujjwalgaihre0@gmail.com>
2025-11-01 17:45:10 +0530

    fix

--- apps/web-forntend/app/draw/user.ts
+++ apps/web-forntend/app/draw/user.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
-import { HTTP_BACKNED } from "../config";
+import { HTTP_BACKEND } from "../config";
 
 export const  Userdata=async()=>{
     const token = localStorage.getItem("token");
     try {
-    const res = await axios.get(`${HTTP_BACKNED}/me`, {
+    const res = await axios.get(`${HTTP_BACKEND}/me`, {
      headers: {
      Authorization: `Bearer ${token}`
       }
